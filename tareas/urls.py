6a6423b80c087defd6bfb6010ba0b870e1adad72
Bubbl33s <jairo.lf.8@gmail.com>
2024-08-02 15:58:33 -0500

    create tareas

--- tareas/urls.py
+++ tareas/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.tareas, name='tareas'),
+    path('crear_tarea/', views.crear_tarea, name='crear_tarea'),
 ]
--- tareas/views.py
+++ tareas/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Tarea
 
 
 def tareas(request):
     return render(request, 'tareas.html')
+
+
+def crear_tarea(request):
+    # Si es un método post, se intenta crear una tarea
+    if request.method == "POST":
+        # Se obtienen los datos del formulario y se guardan en variables
+        # También se pueden pasar los datos del formulario directamente sin crear variables
+        titulo = request.POST.get('titulo')
+        descripcion = request.POST.get('descripcion')
+        es_importante = 'es_importante' in request.POST
+        img = request.FILES.get('img')
+
+        # Se pasan los valores para instanciar un nuevo objeto Tarea
+        nueva_tarea = Tarea(
+            usuario=request.user,
+            titulo=titulo,
+            descripcion=descripcion,
+            es_importante=es_importante,
+            img=img
+        )
+
+        # Un try para el manejo de errores
+        try:
+            nueva_tarea.save()
+
+            return redirect('tareas')
+        except Exception as e:
+            return render(request, 'crear_tarea.html')
+
+    # Si es un método get, se muestra el template
+    return render(request, 'crear_tarea.html')
